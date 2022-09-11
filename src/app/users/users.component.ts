803c38d4c82869d8e7c4a8ff58ec15092b223cd2
evalquote <evalquote@icloud.com>
2022-09-11 16:09:04 +0200

    refactor(service): update user service

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { User } from './user.model';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-users',
@@ -11,14 +11,10 @@ import { User } from './user.model';
 export class UsersComponent implements OnInit {
   users$: Observable<User[]> = of([]);
 
-  constructor(private http: HttpClient) { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
-    const params = new HttpParams()
-      .set('name_like', 'ne')
-      .set('email_like', 'biz')
-
-    this.users$ = this.http.get<User[]>('https://jsonplaceholder.typicode.com/users', { params });
+    this.users$ = this.userService.getUsers();
   }
 
 }
