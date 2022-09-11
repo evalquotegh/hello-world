bdbdc9b488de4b95be3909de9c1284e8f98ca272
evalquote <evalquote@icloud.com>
2022-09-11 16:05:37 +0200

    refactor(model): update user model

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,30 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-
-type User = {
-  id: number;
-  name: string;
-  username: string;
-  email: string;
-  address: {
-    street: string;
-    suite: string;
-    city: string;
-    zipcode: string;
-    geo: {
-      lat: string;
-      lng: string;
-    }
-  },
-    phone: string;
-    website: string;
-    company: {
-    name: string;
-    catchPhrase: string;
-    bs: string;
-  }
-}
+import { User } from './user.model';
 
 @Component({
   selector: 'app-users',
